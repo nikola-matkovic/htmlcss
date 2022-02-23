f52cdd4b7297f049db87ad45a0935df923324b34
anonymousvegan <anonymousvegan>
2022-02-23 16:02:43 +0100

    thet is started well

---
+++ vibration-calculator/app.js
@@ -0,0 +1,25 @@
+let timer = 0, timerInterval;
+const button = document.querySelector("button");
+
+button.addEventListener("mousedown", down);
+button.addEventListener("mouseup", up);
+button.addEventListener("dblclick", reset);
+
+function down(){
+    console.log("im down");
+    timerInterval = setInterval(function(){
+        timer += 1;
+        navigator.vibrate(200);
+    }, 300);
+}
+
+function up(){
+    console.log("im up");
+    clearInterval(timerInterval);
+    console.log(timer);
+    timer = 0;
+}
+
+function reset(){
+    console.log("reseted");
+}
