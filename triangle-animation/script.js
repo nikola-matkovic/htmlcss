ab33680aa3b17bde94bdb2f94764b904e807df69
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-23 23:58:00 +0200

    more random colors

--- triangle-animation/script.js
+++ triangle-animation/script.js
@@ -1,8 +1,14 @@
 const svg = document.querySelector('svg');
 
 svg.addEventListener("click",  (e)  => {
-    const colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink']; 
-    const random = () => colors[Math.floor(Math.random() * colors.length)]
+    let random = () => {
+        const colorPars = {r: 0, g: 0, b: 0}
+        for(i in colorPars){
+            colorPars[i] = Math.floor(Math.random() * 255)
+        }
+        return `rgb(${colorPars.r},${colorPars.g},${colorPars.b})`
+    }
+
     document.documentElement.style.cssText = `
     --dark-color : ${random()};
     --light-color : ${random()};
