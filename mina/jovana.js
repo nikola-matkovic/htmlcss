355a64d1a5d098e7ac9dbd7cb05ac3871e079d59
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-04 11:13:23 +0200

    jovana haker

---
+++ mina/jovana.js
@@ -0,0 +1,12 @@
+let prvi = 1
+let drugi = 3
+let treci =6
+let cetvrti =8
+let peti =2
+let najveci = prvi
+if (drugi>najveci) najveci=drugi
+if (treci>najveci) najveci=treci
+if (cetvrti>najveci) najveci=cetvrti
+if (peti>najveci) najveci=peti
+
+console.log (najveci)
