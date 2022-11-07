0eef0c73668a6bb4d73d395f13c0b2cda76ed85a
anonymousvegan <majjunjulavgust@gmail.com>
2022-11-07 13:54:17 +0100

    one line B)

--- 7-11-2022/script.js
+++ 7-11-2022/script.js
@@ -18,9 +18,22 @@ class  student {
     }
 }
 
+function najbolji(...studenti) {
+    let niz = []
+    studenti.forEach(student => niz.push(student.prosek))
+    console.log(niz);
+    return Math.max(niz);
+}
+
 let student1 = new student("nikola", "Matković", "036016/21")
 
 student1.dodajOcenu(10)
-student1.dodajOcenu(8)
+student1.dodajOcenu(7)
 
-console.log(student1.prosek);
+let student2 = new student("tarik", "kučević", "036016/21")
+
+student2.dodajOcenu(10)
+student2.dodajOcenu(8)
+
+
+console.log(najbolji(student1, student2));
