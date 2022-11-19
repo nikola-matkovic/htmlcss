e36c2090ca276a52c731a6b47cb3702650cb3da2
anonymousvegan <majjunjulavgust@gmail.com>
2022-11-19 11:18:22 +0100

    learning

---
+++ 14-11-2022/v2.js
@@ -0,0 +1,4 @@
+const x = /script/
+
+const text = "Javascrip is computer language"
+let y = text.search(x);
---
+++ 14-11-2022/v4.js
@@ -0,0 +1,6 @@
+let x1 = /20\d\d/g
+
+const y = "1989-ollimpijada 1987 1993 1996-ollimpijada nešto nešto 2012 nešto 1998 1996 2002 2005 2006 2008 olimpijada 2008"
+y1 = y.match(x1)
+
+console.log(y1);
